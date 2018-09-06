0668f1edffe35ce68ef088161cf4a2b1a269c0b2
Marc Sevilleja <marc@sevilayha.com>
2018-09-06 13:39:52 -0700

    LoadScene
    Created a LoadScene script to load another scene after putting in the correct parameters.

---
+++ Unity2D Practice/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+    public string OtherScene;
+
+    public void Start()
+    {
+        
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        SceneManager.LoadScene(OtherScene, LoadSceneMode.Additive);
+    }
+
+    
+}
