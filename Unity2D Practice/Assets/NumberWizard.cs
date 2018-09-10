88376a5fbd0a5586532ef5f143c9aa464dc65bdd
CoolComp <marc@sevilayha.com>
2018-09-09 23:41:20 -0700

    Number Wizard 2.0
    Better UI with Number Wizard 2.0

---
+++ Unity2D Practice/Assets/NumberWizard.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+
+    int max, min, avg;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        Reset();
+        print("========================");
+        print("Welcome to Number Wizard");
+        print("Pick a number in your head, but don't tell me!");
+
+        print("Pick a number between " + min + " and " + max + ".");
+
+        print("Is the number higher or lower than " + max / 2 + "?");
+        print("Up = Higher, Down = Lower, Return = Equal");
+    }
+
+    void Reset()
+    {
+        max = 1000;
+        min = 1;
+    }
+
+    void NextGuess()
+    {
+        avg = Avg(min, max);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        NextGuess();
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = avg;
+            NextGuess();
+
+            if (avg == min)
+            {
+                print("Is your number " + avg + "?");
+            }
+            else if (avg < 999)
+            {
+                print("Your number is greater than " + min);
+                print("Is the number higher or lower than " + avg + "?");
+            }
+            else if (avg == 999)
+            {
+                avg += 1;
+                Won();
+            }
+
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = avg;
+            NextGuess();
+
+            if (avg == max)
+            {
+                print("Is your number " + avg + "?");
+            }
+            else if (avg > 1)
+            {
+                print("Your number is less than " + max);
+                print("Is the number higher or lower than " + avg + "?");
+            }
+            else if (avg == 1)
+                Won();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Won();
+        }
+    }
+
+    int Avg(int minNum, int maxNum)
+    {
+        return (minNum + maxNum) / 2;
+    }
+
+    void Won()
+    {
+        print("I got it! Your number is " + avg + "!");
+        StartGame();
+    }
+}
---
+++ Unity2D Practice/Assets/TextManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextManager : MonoBehaviour {
+
+    int avg, min, max;
+
+    public Text myText, numberText, winText;
+
+	// Use this for initialization
+	void Start () {
+        Reset();
+	}
+
+    void Reset()
+    {
+        myText.text = "Is your number:";
+        numberText.text = "500";
+        //winText.text = " ";
+        max = 1000;
+        min = 1;
+    }
+
+    void NextGuess()
+    {
+        avg = Avg(min, max);
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        NextGuess();
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = avg;
+            NextGuess();
+
+            if (avg == min)
+            {
+                myText.text = "Is your number:";
+                numberText.text = avg + "~~";
+            }
+            else if (avg < 999)
+            {
+                myText.text = "Is the number higher or lower than:";
+                numberText.text = avg + "?";
+            }
+            else if (avg == 999)
+            {
+                avg += 1;
+                Won();
+            }
+
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = avg;
+            NextGuess();
+
+            if (avg == max)
+            {
+                myText.text = "Is your number:";
+                numberText.text = avg + "~~";
+            }
+            else if (avg > 1)
+            {
+                myText.text = "Is the number higher or lower than:";
+                numberText.text = avg + "?";
+            }
+            else if (avg == 1)
+                Won();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Won();
+        }
+    }
+
+    int Avg(int minNum, int maxNum)
+    {
+        return (minNum + maxNum) / 2;
+    }
+
+    void Won()
+    {
+        myText.text = "I got it! Your number is:";
+        numberText.text = avg + "!";
+        winText.text = "Press Enter to start again!";
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Reset();
+        }
+    }
+}
